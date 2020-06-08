b524ff270601e4006852dd0ec09f9efdc5581d87
isama22 <elisabethmartinez@Elisabeths-MacBook-Pro.local>
2020-06-08 01:59:17 -0700

    navbar component in but not ready to be used yet

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Navbar.css';
+import { Link } from 'react-router-dom';
+
+
+const NavBar = (props) => {
+    const nav = props.user ?
+        <div className="nav">
+            <Link to="/" className="home">Home</Link>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <Link to='' onClick={props.handleLogout}>Log out</Link>
+            {/* <Link to="/endpage1">endpage1</Link> */}
+            <span>logged in as: {props.user.name}</span>
+        </div>
+        :
+        <div className="nav">
+            <Link to='/login'>Log in</Link>
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <Link to='/signup'>Sign up</Link>
+        </div>
+    return (
+        <div className='NavBar'>
+            {nav}
+        </div>
+    )}
+export default NavBar;
